db1c1c2595c3d68a0c10084f86dd44ac1eb89315
Danyael Camacho <danyisxdlool45@gmail.com>
2018-05-28 01:28:12 -0500

    1subida
    1subida

---
+++ js/acciones.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready", function(){
+		$('#izquierda').on("swipeleft",function(){
+			navigator.notification.alert("Deslizó a la izquierda",function(){"Aplicación 7","Aceptar"});
+}); //cierre deslizar izquierda
+
+		$('#derecha').on("swiperight",function(){
+			navigator .notification. confirm("¿Qué quieres hacer?",function(opt){
+				switch(opt)
+				{
+					case 1:
+					navigator.notification.beep(2);
+					break;
+					
+					case 2:
+					navigator.notification.vibrate(2000);
+					break;
+				}
+			},"Aplicación 7","Beepear,Vibrar,Cancelar");
+			
+		});
+	},false);
+});
+
